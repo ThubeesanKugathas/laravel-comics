06dea7851e1c1d9b88db29f206cf90ffc982c161
ThubeesanKugathas <Thubeesan@outlook.com>
2022-03-03 14:35:21 +0100

    navbar + init images in public folder

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,39 @@
+<div class="ms_left">
+    <img src="{{ url('img/dc-logo.png') }}" alt="dc-logo">
+</div>
+
+<!-- bar right side del flex container -->
+<div class="ms_right">
+    <ul>
+        <li>
+            <a href="/#">CHARACTERS</a>
+        </li>
+        <li>
+            <a href="/#">COMICS</a>
+        </li>
+        <li>
+            <a href="/#">MOVIES</a>
+        </li>
+        <li>
+            <a href="/#">TV</a>
+        </li>
+        <li>
+            <a href="/#">GAMES</a>
+        </li>
+        <li>
+            <a href="/#">COLLECTIBLES</a>
+        </li>
+        <li>
+            <a href="/#">VIDEOS</a>
+        </li>
+        <li>
+            <a href="/#">FANS</a>
+        </li>
+        <li>
+            <a href="/#">NEWS</a>
+        </li>
+        <li>
+            <a href="/#">SHOP</a>
+        </li>
+    </ul>
+</div>
